4ca8ed112885fb0e19760eee84dc2d75d6af518b
Joao Melo <jopcmelo@gmail.com>
2021-05-05 19:50:43 -0300

    feat: calculate the summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,7 +6,26 @@ import totalImg from "../../assets/total.svg";
 import { Container } from "./styles";
 
 export function Summary() {
-  const transactions = useContext(TransactionsContext);
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
 
   return (
     <Container>
@@ -15,21 +34,37 @@ export function Summary() {
           <p>Incomes</p>
           <img src={incomeImg} alt="Incomes" />
         </header>
-        <strong>R$1.000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "USD",
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Outcomes</p>
           <img src={outcomeImg} alt="Outcomes" />
         </header>
-        <strong>-R$500,00</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "USD",
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "USD",
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
